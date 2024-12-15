88bccd162d2c187211f632bb65907c36ed991efd
unknown <k.ivanov2020@pmg-pernik.com>
2024-12-15 14:46:54 +0200

    ok

---
+++ DOGGApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DOGGApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        internal readonly object Dogs;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
